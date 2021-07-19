3797abb6f3b9eeff6e15afbf3e2db466d6126bdc
Jenny Kim <kimj1905@gmail.com>
2021-07-19 15:00:26 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Author.create!(name: "Judy Blume", genre: "Fiction", bio: "Judy Blume was a pioneer in children's fiction novels.")
